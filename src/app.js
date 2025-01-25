1256138ba773591de3fe28ff3e4d2c37b4b70baf
AhmedHamed255 <ahmedcina60@yahoo.com>
2025-01-25 16:40:23 +0200

    update swagger css

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./config/swaggerOptions");
 const swaggerJSON = require("../public/swagger.json");
 const app = express();
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 // Middlewares
 app.use(express.json());
@@ -12,12 +13,15 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Swagger documentation route
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJSON, {
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerJSON, {
     customCss:
-        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
     customCssUrl: CSS_URL,
-}
-))
+  })
+);
 //Endpoints Middlewares
 app.use("/auth", require("./modules/auth/auth.router"));
 app.use("/user", require("./modules/user/user.router"));
